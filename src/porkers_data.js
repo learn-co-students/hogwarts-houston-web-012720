068a370e8b24389e0517851aa3ee672e3c5d20d0
somaia-khalil <sumaya_khair@live.com>
2020-04-03 14:15:41 -0500

    Done.

--- src/porkers_data.js
+++ src/porkers_data.js
@@ -1,94 +1,122 @@
+import pig1 from "./hog-imgs/augustus_gloop.jpg"
+import pig2 from "./hog-imgs/bay_of_pigs.jpg"
+import pig3 from "./hog-imgs/cherub.jpg"
+import pig4 from "./hog-imgs/galaxy_note.jpg"
+import pig5 from "./hog-imgs/leggo_my_eggo.jpg"
+import pig6 from "./hog-imgs/mudblood.jpg"
+import pig7 from "./hog-imgs/piggy_smalls.jpg"
+import pig8 from "./hog-imgs/porkchop.jpg"
+import pig9 from "./hog-imgs/rainbowdash.jpg"
+import pig10 from "./hog-imgs/sobriety.jpg"
+import pig11 from "./hog-imgs/the_prosciutto_concern.jpg"
+import pig12 from "./hog-imgs/trouble.jpg"
+import pig13 from "./hog-imgs/truffleshuffle.jpg"
+
+
 const hogs = [
   {
     name: 'Mudblood',
     specialty: 'Mediocre magic',
     greased: false,
     weight: 2.0,
-    'highest medal achieved': 'bronze'
+    'highest medal achieved': 'bronze',
+     img: pig1
   },
   {
     name: 'Porkchop',
     specialty: 'Making friends',
     greased: true,
     weight: 1.6,
-    'highest medal achieved': 'silver'
+    'highest medal achieved': 'silver',
+    img: pig2
   },
   {
     name: 'Cherub',
     specialty: 'truffle finder',
     greased: false,
     weight: 0.7,
-    'highest medal achieved': 'gold'
+    'highest medal achieved': 'gold',
+    img: pig3
   },
   {
     name: 'Piggy smalls',
     specialty: 'Massivity',
     greased: true,
     weight: 5.1,
-    'highest medal achieved': 'platinum'
+    'highest medal achieved': 'platinum',
+    img:pig4
   },
   {
     name: 'Trouble',
     specialty: 'Racing',
     greased: true,
     weight: 1.7,
-    'highest medal achieved': 'gold'
+    'highest medal achieved': 'gold',
+    img: pig5
   },
   {
     name: 'Sobriety',
     specialty: 'Expert Witness',
     greased: false,
     weight: 2.2,
-    'highest medal achieved': 'silver'
+    'highest medal achieved': 'silver',
+    img: pig6
   },
   {
     name: 'Rainbowdash',
     specialty: 'Union Busting',
     greased: false,
     weight: 3.7,
-    'highest medal achieved': 'wood'
+    'highest medal achieved': 'wood',
+    img: pig7
   },
   {
     name: 'TruffleShuffle',
     specialty: 'Finding truffles',
     greased: true,
     weight: 4.0,
-    'highest medal achieved': 'gold'
+    'highest medal achieved': 'gold',
+    img:pig8
   },
   {
     name: 'Bay of Pigs',
     specialty: 'Hamphlet Distribution',
     greased: false,
     weight: 2.8,
-    'highest medal achieved': 'diamond'
+    'highest medal achieved': 'diamond',
+    img:pig9
   },
   {
     name: 'The Prosciutto Concern',
     specialty: 'truffle finder',
     greased: false,
     weight: 2.3,
-    'highest medal achieved': 'bronze'
+    'highest medal achieved': 'bronze',
+    img:pig10
   },
   {
     name: 'Galaxy Note',
     specialty: 'Airport Security',
     greased: true,
     weight: 1.9,
-    'highest medal achieved': 'diamond'
+    'highest medal achieved': 'diamond',
+    img:pig11
   },
   {
     name: 'Leggo My Eggo',
     specialty: 'Babysitting',
     greased: true,
     weight: 3.3,
-    'highest medal achieved': 'platinum'
+    'highest medal achieved': 'platinum',
+    img:pig12
   },
   {
     name: 'Augustus Gloop',
     specialty: 'Vacuum tubes',
     greased: true,
     weight: 4.1,
-    'highest medal achieved': 'wood'
+    'highest medal achieved': 'wood',
+    img:pig13
   }
 ];
 
