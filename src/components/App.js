c22d151f22670ac394ca221933fb2d6b4812be73
placidowang <placidow@gmail.com>
2020-04-03 02:19:47 -0500

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,88 @@ import React, { Component } from "react";
 import "../App.css";
 import Nav from "./Nav";
 import hogs from "../porkers_data";
-import HelloWorld from "./HelloWorld";
+// import HelloWorld from "./HelloWorld";
+import Filter from './Filter.js'
+import Sort from './Sort.js'
+import HogBrowser from './HogBrowser.js'
 
 class App extends Component {
+  state = {
+    hogs,
+    greaseFilter: false,
+    sort: 'default'
+  }
+
+  filterHogs = (hogs) => {
+    return this.state.greaseFilter
+    ? hogs.filter(hog => hog.greased === true)
+    : hogs
+  }
+
+  showHogs = () => {
+    let filteredHogs = this.filterHogs(this.state.hogs)
+    let showHogs = []
+
+    switch (this.state.sort) {
+      case 'default':
+        console.log('filtering by default')
+        showHogs = filteredHogs
+        break
+      case 'name':
+        console.log('filtering by name')
+        showHogs = [...filteredHogs].sort((a,b) => (a.name > b.name) ? 1 : -1)
+        // console.log(this.state.hogs)
+        break
+      case 'weight':
+        console.log('filtering by weight')
+        showHogs = [...filteredHogs].sort((a,b) => (a.weight < b.weight) ? 1 : -1)
+        break
+      default:
+        console.log('unknown filter???')
+    }
+    return showHogs
+  }
+
+  toggleFilter = () => {
+    this.setState((prevState) => ({
+      greaseFilter: !prevState.greaseFilter
+    }),
+    ()=>{console.log(`Grease filter: ${this.state.greaseFilter}`)}
+    )
+  }
+
+  // filterHogs = () => {
+  //   const greasedHogs = hogs.filter(hog => hog.greased === true);
+  //   console.log(greasedHogs)
+  //   this.state.greaseFilter
+  //   ? this.setState({hogs: greasedHogs})
+  //   : this.setState({hogs: hogs})
+  // }
+
+  sortHogs = (sortType) => {
+    this.setState({
+      sort: sortType
+    }
+    // ()=>{console.log(this.state.sort)}
+    )
+  }
+
   render() {
+    // console.log(hogs)
     return (
       <div className="App">
         <Nav />
-        <HelloWorld />
+        <Filter
+          toggleFilter={this.toggleFilter} />
+        <div style={{height: '30px'}}></div>
+        <Sort
+          sort={this.state.sort}
+          sortHogs={this.sortHogs}/>
+        {/* <HelloWorld /> */}
+        <div style={{height: '30px'}}></div>
+        <HogBrowser
+          hogs={this.showHogs()}
+          greaseFilter={this.state.greaseFilter} />
       </div>
     );
   }
---
+++ src/components/HogBrowser.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Hog from './Hog.js'
+
+export default class HogBrowser extends React.Component {
+  render() {
+    // console.log(this.props.hogs)
+    // const hogs = this.props.hogs
+    // const greasedHogs = hogs.filter(hog => hog.greased === true);
+    return (
+      // this.props.greaseFilter
+      // ? <div className='ui grid container'>
+      //     {greasedHogs.map(hog => <Hog hog={hog} />)}
+      //   </div>
+      // : <div className='ui grid container'>
+      //     {hogs.map(hog => <Hog hog={hog} />)}
+      //   </div>
+      <div className='ui grid container'>
+        {this.props.hogs.map(hog => <Hog hog={hog} />)}
+      </div>
+    )
+  }
+}
